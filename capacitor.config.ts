6a90263d8403d3f5f19f12f509bb5f5a210f9e32
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-09-17 20:20:04 -0300

    refac: nome do projeto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app-conecta-amor',
+  appName: 'donation-ha-mobile-v2',
   webDir: 'www'
 };
 
